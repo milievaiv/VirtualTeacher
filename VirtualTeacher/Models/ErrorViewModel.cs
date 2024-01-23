cacdbbc0a09d6330fdee67e1b2bf86748b8cca4b
Maria <109968941+milievaiv@users.noreply.github.com>
2024-01-23 18:31:10 +0200

    Create solution for project

---
+++ VirtualTeacher/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VirtualTeacher.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
