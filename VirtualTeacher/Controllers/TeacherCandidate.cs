192df83f922549286d1e5f51fbd843014bd6e9c4
Maria <109968941+milievaiv@users.noreply.github.com>
2024-02-15 21:18:59 +0200

    FIx list-group in the lecture accordion

---
+++ VirtualTeacher/Controllers/TeacherCandidate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualTeacher.Controllers
+{
+    public class TeacherCandidate : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Candidature()
+        {
+            return View();
+        }
+    }
+}
