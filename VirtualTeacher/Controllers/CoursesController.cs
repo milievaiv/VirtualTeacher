401b5cee134034f08b30837a4e12b4cf32454be4
kafaldzhievakaterina <katerina.kafaldzhieva@gmail.com>
2024-02-21 00:40:51 +0200

    Add Create Course View

--- VirtualTeacher/Controllers/CoursesController.cs
+++ VirtualTeacher/Controllers/CoursesController.cs
@@ -1,12 +1,15 @@
 ﻿using AutoMapper;
+using Google.Apis.Drive.v3.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using VirtualTeacher.Attributes;
 using VirtualTeacher.Exceptions;
 using VirtualTeacher.Models;
+using VirtualTeacher.Models.DTO.CourseDTO;
 using VirtualTeacher.Models.ViewModel.CourseViewModel;
 using VirtualTeacher.Services;
 using VirtualTeacher.Services.Contracts;
@@ -20,22 +23,31 @@ namespace VirtualTeacher.Controllers
         private readonly ITeacherService teacherService;
         private readonly IUserService userService;
         private readonly IStudentService studentService;
+        private readonly ICourseTopicService courseTopicService;
         private readonly IMapper mapper;
+        private readonly CloudStorageService cloudStorageService;
         public CoursesController(
             ICourseService courseService,
             ILectureService lectureService,
             ITeacherService teacherService,
             IUserService userService,
             IStudentService studentService,
-            IMapper mapper)
+            ICourseTopicService courseTopicService,
+            IMapper mapper,
+            CloudStorageService cloudStorageService
+            )
         {
             this.courseService = courseService;
             this.lectureService = lectureService;
             this.teacherService = teacherService;
             this.userService = userService;
             this.studentService = studentService;
+            this.courseTopicService = courseTopicService;
             this.mapper = mapper;
+            this.cloudStorageService = cloudStorageService;
+
         }
+
 
         [AllowAnonymous]
         [HttpGet]
@@ -47,6 +59,72 @@ namespace VirtualTeacher.Controllers
             return View(viewModels);
         }
 
+        [AuthorizeUsers("teacher")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var viewModel = new CourseCreateViewModel();
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CoursePicture(int courseId)
+        {
+            var course = courseService.GetById(courseId);
+            var pfpName = course.Id + ".png";
+
+            // Load profile picture data
+            byte[] imageData = cloudStorageService.GetImageContent(pfpName, "course-pictures/");
+
+            return File(imageData, "image/png");
+        }
+
+        [AuthorizeUsers("teacher")]
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateViewModel model)
+        {
+
+            var user = HttpContext.User;
+            var email = user.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email)?.Value;
+            var teacher = teacherService.GetByEmail(email);
+
+            if (ModelState.IsValid)
+            {
+                var course = mapper.Map<Course>(model);
+
+                var coursetopic = courseTopicService.GetAll().FirstOrDefault(x => x.Topic == model.CourseTopic);
+                if (coursetopic == null)
+                {
+                    ModelState.AddModelError("CourseTopic", "Course topic not found.");
+                    // Populate any required data for the view here before returning
+                    return View(model);
+                }
+
+                var courseDto = new CreateCourseDto
+                {
+                    Title = course.Title,
+                    Description = course.Description,
+                    StartDate = course.StartDate,
+                    CourseTopicId = coursetopic.Id,
+                };
+
+                var _course = courseService.Create(courseDto, teacher);
+
+                string newFileName = _course.Id + ".png";
+                if (model.Photo != null && model.Photo.Length > 0)
+                {
+                    using (var fileStream = model.Photo.OpenReadStream())
+                    {
+                        // Use the dynamically fetched bucket name
+                        await cloudStorageService.UploadFileAsync("course-pictures/" + newFileName, fileStream);
+                    }
+                }
+
+                return RedirectToAction("Index", "Courses");
+            }
+
+            return View(model);
+        }
 
         [AllowAnonymous]
         [HttpGet("courses/GetCoursesByTopic")]
@@ -64,37 +142,63 @@ namespace VirtualTeacher.Controllers
             return PartialView("_CoursesByTopicPartial", viewModels);
         }
 
-        [AuthorizeUsers("student")]
+        [AllowAnonymous]
         [HttpGet("courses/{id}/details")]
         public IActionResult Details([FromRoute] int id)
         {
-            var user = HttpContext.User;
-            var email = user.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email)?.Value;
-            var student = studentService.GetByEmail(email);
+            Student student = null;
+            var jwtFromRequest = Request.Cookies["Authorization"];
+
+            if (!string.IsNullOrEmpty(jwtFromRequest))
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                try
+                {
+                    var jwtToken = tokenHandler.ReadToken(jwtFromRequest) as JwtSecurityToken;
+                    if (jwtToken != null)
+                    {
+                        var emailClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+                        if (emailClaim != null)
+                        {
+                            student = studentService.GetByEmail(emailClaim.Value);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // This ensures that any issues with token processing do not prevent the method from proceeding
+                }
+            }
 
             try
             {
-                var course = courseService.GetById(id);   
+                var course = courseService.GetById(id);
                 if (course == null)
                 {
                     return View("NotFoundError");
                 }
 
                 var topCourses = courseService.GetAll()
-                .OrderByDescending(x => x.Ratings.Any() ? x.Ratings.Average(r => r.RatingValue) : 0)
-                .ToList();
+                    .OrderByDescending(x => x.Ratings.Any() ? x.Ratings.Average(r => r.RatingValue) : 0)
+                    .ToList();
 
                 var courseViewModel = mapper.Map<CourseViewModel>(course);
                 courseViewModel.TopCourses = mapper.Map<IEnumerable<CourseViewModel>>(topCourses);
-
-                bool isEnrolled = student.EnrolledCourses.Any(x => x.CourseId == id);
-                ViewBag.IsEnrolled = isEnrolled;
-                ViewBag.CourseId = id; 
 
                 if (course.Ratings != null && course.Ratings.Any())
                 {
                     courseViewModel.AverageRating = course.Ratings.Average(r => r.RatingValue);
                 }
+
+                if (student != null)
+                {
+                    bool isEnrolled = student.EnrolledCourses.Any(x => x.CourseId == id);
+                    ViewBag.IsEnrolled = isEnrolled;
+                   
+                }              
+
+                ViewBag.CourseId = id; 
 
                 return View(courseViewModel);
             }
@@ -103,6 +207,7 @@ namespace VirtualTeacher.Controllers
                 return View("NotFoundError");
             }
         }
+
 
         [AuthorizeUsers("student")]
         [HttpGet("courses/{id}/lectures")]
@@ -135,20 +240,6 @@ namespace VirtualTeacher.Controllers
             }
         }
 
-        //[HttpPost]
-        //public ActionResult IncrementProgress(int courseId)
-        //{
-        //    var user = HttpContext.User;
-        //    var email = user.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email)?.Value;
-        //    var student = studentService.GetByEmail(email);
-        //    var course = courseService.GetById(courseId);
-
-        //    double? currentProgress = studentService.CalculateProgress(student, course);
-        //    currentProgress++;
-
-        //    return RedirectToAction("Lectures", new { courseId = courseId });
-        //}
-
 
         [AuthorizeUsers("student")]
         [HttpGet]
@@ -177,22 +268,11 @@ namespace VirtualTeacher.Controllers
             var user = HttpContext.User;
             var email = user.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email)?.Value;
             var student = studentService.GetByEmail(email);
-            // var isCourseCompleted = student.CompletedCourses.Any(x => x.CourseId == courseId);
 
             courseService.RateCourse(courseId, student.Id, rating, feedback);
 
-            return RedirectToAction("Details", new { id = courseId });
+            return RedirectToAction("Details", new { id = courseId });     
 
-            //if (isCourseCompleted)
-            //{
-               
-            //}
-            //else
-            //{
-            //    return Conflict("You must have completed the course to rate it.");
-            //}          
-
-        }
-       
+        }       
     }
 }
--- VirtualTeacher/Services/Google Service/CloudStorageService.cs
+++ VirtualTeacher/Services/Google Service/CloudStorageService.cs
@@ -79,5 +79,6 @@ public class CloudStorageService
             null,
             fileStream
         );
-    }
+    }   
+
 }
